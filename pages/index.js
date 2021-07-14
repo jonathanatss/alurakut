1136242633034cf69d3bf155fc4f358583442510
Jonathan Alberto <jonathanatss@gmail.com>
2021-07-14 02:01:28 -0300

    master

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 //const Title = styled.h1`
@@ -9,16 +10,35 @@ import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 
 function ProfileSidebar(props) {
-  console.log(props);
+
   return (
     <Box>
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
-    </Box>
+      <hr/>
+    <p>
+      <a className ="boxLink" href={`https://github.com/${props.githubUser}`}>
+        @{props.githubUser}
+        </a>
+      </p>
+      <hr/>
+      
+
+      <AlurakutProfileSidebarMenuDefault/>
+   </Box>
   )
 }
  
 export default function Home() {
+  const [comunidades, setComunidades] = React.useState([{
+    id: '129810217291728167291628152987165216725162512',
+    title: 'T.D.A - Traumas da Adolescência',
+    image: 'https://bit.ly/3i50rat'
+  }]);
+  
+  console.log('Nosso teste', comunidades);
+  
   const usuarioAleatorio = 'jonathanatss';
+// const comunidades = ['Alurakut'];
   const pessoasFavoritas = [
    'juunegreiros',
    'omariosouto',
@@ -44,20 +64,78 @@ export default function Home() {
 
              <OrkutNostalgicIconSet/>
             </Box>
+            <Box>
+              <h2 className="subTitle">O que você deseja fazer?</h2>
+              <form onSubmit={function handleCriaComunidade(e) {
+                e.preventDefault();
+                const dadosDoForm = new FormData(e.target);
+
+                console.log('Campo: ', dadosDoForm.get('title'));
+                console.log('Campo: ', dadosDoForm.get('image'));
+
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get('title'),
+                  image: dadosDoForm.get('image'),
+                }
+
+                const comunidadesAtualizadas = [...comunidades, comunidade]
+                setComunidades(comunidadesAtualizadas)
+              }}>
+                  <div>
+                    <input 
+                      placeholder="Qual vai ser o nome da sua comunidade?"
+                      name="title" 
+                      aria-label="Qual vai ser o nome da sua comunidade?"
+                      type="text"
+                      />
+                  </div>
+                  <div>
+                    <input 
+                      placeholder="Coloque uma URL para usarmos de capa"
+                      name="image" 
+                      aria-label="Coloque uma URL para usarmos de capa"/>
+                  </div>
+
+                  <div>
+                    <button>
+                      Criar comunidade
+                    </button>
+                  </div>
+              </form>
+              
+            </Box>
         </div>
       <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea '}}>
+      <ProfileRelationsBoxWrapper>
+         <h2 className="smallTitle">
+          Comunidades ({comunidades.length})
+          </h2>
+          <ul>
+          {comunidades.map((itemAtual) => {
+            return (
+              <li key={itemAtual.id}>
+                <a href={`/users/${itemAtual.title}`} >
+                   <img src={itemAtual.image} />
+                <span>{itemAtual.title}</span>
+                </a>
+              </li>
+              
+            )
+          })}
+      </ul>
+      </ProfileRelationsBoxWrapper>
       <ProfileRelationsBoxWrapper>
         <h2 className="smallTitle">
           Pessoas da Comunidade ({pessoasFavoritas.length})
         </h2>
       
-
       <ul>
           {pessoasFavoritas.map((itemAtual) => {
             return (
 
-              <li>
-                <a href={`/users/${itemAtual}`} key={itemAtual}>
+              <li key={itemAtual}>
+                <a href={`/users/${itemAtual}`} >
                 <img src={`https://github.com/${itemAtual}.png`} />
                 <span>{itemAtual}</span>
               </a>
--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+
+
 const MainGrid = styled.main`
   width: 100%;
   grid-gap: 10px;
